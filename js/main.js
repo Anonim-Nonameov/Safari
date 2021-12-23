5400d64c9c496e495591ea79d2c4755be3c232cd
Аноним <bratprogrammist@Gmail.com>
2021-12-23 21:08:13 +0400

    UPDATE - ЗАВЕРШЕНИЕ ПОПАП СЕКЦИИ И ФОТОГАЛЕРЕИ (частично, но на 90%)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 $(function(){
+  $('.popup__link').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false
+	});
 
+	$('.gallery__slider').slick({
+		prevArrow: '<button type="button" class="slick-btn slick-prev"> <img src="images/arrow-left.svg" alt="Картинка"></button>',
+		nextArrow: '<button type="button" class="slick-btn slick-next"> <img src="images/arrow-right.svg" alt="Картинка"></button>'
+	});
 });
